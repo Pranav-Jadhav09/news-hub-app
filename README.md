a820441323a16b3bcc6c57d500f44843715851bc
Pranav Rajesh Jadhav <jhadavpranav@gmail.com>
2023-10-03 13:17:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+<div align="center">
+
+![GitHub repo size](https://img.shields.io/github/repo-size/Pranav-Jadhav09/currency-converter-app)
+[![Twitter Follow](https://img.shields.io/twitter/follow/Pranav_Jadhav09?style=social)](https://twitter.com/Pranav_Jadhav09)
+
+# Ne A
+
+This project is a simple news app that allows users to search for news articles based on keywords and view the latest news.<br />
+<a href="https://ns-hu-ap-site.onrender.com/"><strong>➥ Live Demo</strong></a>
+
+</div>
+
+<br/>
+
+## Introduction
+
+This news app provides a user-friendly interface to browse and search for news articles. It leverages the News API to fetch real-time news data based on user queries.
+
+<br/>
+
+## Features
+
+- `Search`: Users can search for news articles based on keywords or topics of interest.
+- `Responsive Design`: The app is designed to be accessible and usable on various devices, including desktops, tablets, and mobile phones.
+- `Navigation`: The app includes a navigation menu to easily navigate through different sections of the application.
+
+<br/>
+
+## Technologies
+
+- HTML5
+- CSS3
+- JavaScript
+- News API
+
+<br/>
+
+## Setup and Installation
+
+- Clone this repository to your local machine using git clone.
+- Obtain a free API key from [News API](https://newsapi.org) by signing up for an account.
+- Replace the `apiKey` variable in script.js with your obtained API key.
+- Open `index.html` in a web browser to run the application.
+
+<br/>
+
+## Usage
+
+- Open the app in a web browser.
+- Use the navigation menu to browse different sections or search for news articles using the search feature.
+- Enter a keyword or topic in the search bar and click `"Search"` to fetch news articles related to that keyword.
+- Click on any article to read the full details.
+
+<br/>
+
+## Contributing
+
+Contributions to this project are welcome! Feel free to submit pull requests or open issues for any improvements or features you'd like to add.
+
+<br />
+
+## License
+
+This project is licensed under [MIT License](./LICENSE).
---
+++ assets/js/app.js
@@ -0,0 +1,119 @@
+"use strict";
+
+const apiKey = `1dd1b901c6c94851921ef21454e7c3ac`;
+const url = "https://newsapi.org/v2/everything?q=";
+
+// Call the function to load window
+window.addEventListener("load", () => fetchNews("India"));
+
+//////////////////////
+// Responsive navbar
+const hamIcon = document.querySelector(".ham-icon");
+const mobileNav = document.querySelector(".mobile-navbar");
+
+hamIcon.addEventListener("click", () => {
+  const icon = document.querySelector(".ham-icon i");
+  icon.classList.toggle("ri-menu-3-fill");
+  icon.classList.toggle("ri-close-circle-fill");
+
+  document.querySelector(".mobile-navbar").style.display = "flex";
+
+  if (icon.classList == "ri-menu-3-fill") {
+    document.querySelector(".mobile-navbar").style.display = "none";
+  }
+});
+
+//////////////////////
+// Reload function
+const reload = () => {
+  window.location.reload();
+};
+
+/////////////////////////////////
+// Async function to fetch news
+const fetchNews = async (query) => {
+  try {
+    const result = await fetch(`${url}${query}&apikey=${apiKey}`);
+    const data = await result.json();
+    // console.log(data);
+    bindData(data.articles);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+  }
+};
+
+//////////////////////
+// Bind Data function
+const bindData = (articles) => {
+  const cardsContainer = document.getElementById("cards-container");
+  const newscardTemplate = document.getElementById("template-news-card");
+
+  cardsContainer.innerHTML = "";
+
+  articles.forEach((article) => {
+    if (!article.urlToImage) return;
+
+    const cardClone = newscardTemplate.content.cloneNode(true);
+    fillDataInCard(cardClone, article);
+    cardsContainer.appendChild(cardClone);
+  });
+};
+
+// Fill Data in card
+const fillDataInCard = (cardClone, article) => {
+  const newsImg = cardClone.querySelector("#news-image");
+  const newsTitle = cardClone.querySelector("#news-title");
+  const newsSource = cardClone.querySelector("#news-source");
+  const newsDesc = cardClone.querySelector("#news-desc");
+
+  newsImg.src = article.urlToImage;
+  newsTitle.innerHTML = article.title;
+  newsDesc.innerHTML = article.description;
+
+  // Getting date
+  const date = new Date(article.publishedAt).toLocaleString("en-US", {
+    timeZone: "Asia/Jakarta",
+  });
+
+  newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+  // Onclick sent to the article.url
+  cardClone.firstElementChild.addEventListener("click", () => {
+    window.open(article.url, "_blank");
+  });
+};
+
+//////////////////////
+// Active nav links
+let activeSelectedNav = null;
+
+const onNavItemClick = (id) => {
+  fetchNews(id);
+  const navItem = document.getElementById(id);
+  activeSelectedNav?.classList.remove("active");
+  activeSelectedNav = navItem;
+  activeSelectedNav.classList.add("active");
+};
+
+//////////////////////
+// Search Btn feature
+const searchBtn = document.getElementById("search-btn");
+const searchText = document.getElementById("search-text");
+const mobileSearchBtn = document.getElementById("mobile-search-btn");
+const mobileSearchText = document.getElementById("mobile-search-text");
+
+searchBtn.addEventListener("click", () => {
+  const query = searchText.value;
+
+  if (!query) return;
+
+  fetchNews(query);
+});
+
+mobileSearchBtn.addEventListener("click", () => {
+  const query = mobileSearchText.value;
+
+  if (!query) return;
+
+  fetchNews(query);
+});
